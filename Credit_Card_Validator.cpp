dff64a93418dd0ba8d1f5051d36469c2e03373cb
all4nitrous <all4nitrous@gmail.com>
2025-02-14 12:27:09 -0500

    functions and namespaces added

--- Credit_Card_Validator.cpp
+++ Credit_Card_Validator.cpp
@@ -8,3 +8,38 @@
     4. Sum results from steps 2 & 3
     5. If step 4 is divisble by 10, # is valid
 */
+#include <iostream>
+
+// Namespaces
+using std::cout;
+using std::endl;
+using std::cin;
+using std::string;
+
+//Functions
+int getDigit(const int number);
+int sumOddDigits(const string cardNumber);
+int sumEvenDigits(const string cardNumber);
+
+int main()
+{
+
+
+
+    return;
+}
+
+int getDigit(const int number)
+{
+
+}
+
+int sumOddDigits(const string cardNumber)
+{
+
+}
+
+int sumEvenDigits(const string cardNumber)
+{
+    
+}
